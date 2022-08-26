835c1000fff214f9b1a2ae59f9b35d712dafed35
PierreSQS <nomo_messina@web.de>
2022-08-26 15:48:20 +0200

    Sec3_Chap20-SpringBoot Profile Assn-INC: related code 6

--- src/main/java/guru/springframework/bootstrap/profilesysout/DevProfileSysOut.java
+++ src/main/java/guru/springframework/bootstrap/profilesysout/DevProfileSysOut.java
@@ -1,5 +1,6 @@
 package guru.springframework.bootstrap.profilesysout;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Component;
 @Profile("dev")
 public class DevProfileSysOut {
 
+    @Autowired
     public DevProfileSysOut(@Value("${guru.springframework.profile.message}") String msg) {
         System.out.println("##################################");
         System.out.println("##################################");
