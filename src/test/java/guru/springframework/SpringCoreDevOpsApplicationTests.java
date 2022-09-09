9fac0eb69e39df063d0facd048a242cce0930e44
PierreSQS <nomo_messina@web.de>
2022-09-10 00:21:54 +0200

    Sec5_Chap50-GitHub WebHooks: testing jenkins webhooks

--- src/test/java/guru/springframework/SpringCoreDevOpsApplicationTests.java
+++ src/test/java/guru/springframework/SpringCoreDevOpsApplicationTests.java
@@ -2,17 +2,23 @@ package guru.springframework;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SpringCoreDevOpsApplication.class)
-@WebAppConfiguration
+@SpringBootTest
 public class SpringCoreDevOpsApplicationTests {
+
+	@Autowired
+	ApplicationContext appCtx;
 
 	@Test
 	public void contextLoads() {
+		assertThat(appCtx).isNotNull();
 	}
 
 }
